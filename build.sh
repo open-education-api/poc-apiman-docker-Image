af26ed2c665cd8b92635f6e489471c0cb4c44118
Ton Swieb <ton.swieb@finalist.nl>
2016-01-22 15:42:04 +0100

    Align readme and build script with Docker Hub. Supplying command line arguments during the build is no longer necessary and not supported by Docker Hub

--- build.sh
+++ build.sh
@@ -1,29 +1,11 @@
 #!/bin/sh
 #
-# Using a build script, because the Docker plugin does not work!
 # Alway pull-in the latest parent so we are up to date with latest patch releases
 #
-# Usage: ./build.sh [push]
+# Usage: ./build.sh
 #
-# The [push] option should only be used for Jenkins builds and not local builds.
-#
-DOCKER_IMAGE_NAME=docker-registry.finalist.nl:5000/surfnet/ooapi-poc
-DOCKER_IMAGE_VERSION=1.0
+DOCKER_IMAGE_NAME=surfnet/ooapi-apiman
+DOCKER_IMAGE_VERSION=latest
 
-display_usage() {
-	echo "Builds the Docker image $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION" 
-	echo "\nUsage:\n$0 [admin-password] \n" 
-	echo "admin-password: The password to set for the admin user. Defaults to admin123!"
-	} 
-
-if [ $# -le 0 ]; then 
-	docker rmi --force=true $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION
-	docker build --pull=true --force-rm=true --rm=true -t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION $(dirname $0)
-elif [ $# -le 1 ]; then
-	docker rmi --force=true $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION
-	docker build --pull=true --force-rm=true --rm=true --build-arg ADMIN_PASSWORD=$1 -t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION $(dirname $0)
-else
-	display_usage
-	exit 1
-fi 
-
+docker rmi --force=true $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION
+docker build --pull=true --force-rm=true --rm=true -t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION $(dirname $0)
